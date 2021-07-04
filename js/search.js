0a1ba8884f6d8318192f9dffa5a2add9f2bac700
XAHTE <XAHTE@Motherbase>
2021-07-04 13:14:00 +0500

    viewer, handler and visual improvements (js)

--- js/search.js
+++ js/search.js
@@ -1,7 +1,11 @@
 $(document).on('keyup', '#search_formula', delay(async function () {
     let search_string = $(this).val();
+    if(search_string === ''){
+        search_string = '%';
+    }
     let filter_type = $('#search_filters>button.active').data('filter_type');
-    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type]);
+    let sort = $('#search_sort').data('search_sort');
+    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type, sort]);
     $('#search_results').html(data['content']);
     $('#search_more').html(data['more']);
 }, 500));
@@ -11,17 +15,45 @@ $(document).on('click', '#search_filters>button:not(.active)', async function ()
     $('#search_filters>button.active').removeClass('active');
     $(this).addClass('active');
     let search_string = $('#search_formula').val();
+    if(search_string === ''){
+        search_string = '%';
+    }
     let filter_type = $(this).data('filter_type');
-    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type]);
+    let sort = $('#search_sort').data('search_sort');
+    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type, sort]);
     $('#search_results').html(data['content']);
     $('#search_more').html(data['more']);
 });
 
+$(document).on('click', '#search_sort', async function () { 
+    if($(this).data('search_sort') === 'DESC'){
+        $(this).data('search_sort', 'ASC');
+        $(this).find('svg').css('transform', 'rotate(180deg)');
+    }else{
+        $(this).data('search_sort', 'DESC');
+        $(this).find('svg').css('transform', 'rotate(0deg)');
+    }
+    let search_string = $('#search_formula').val();
+    if(search_string === ''){
+        search_string = '%';
+    }
+    let filter_type = $('#search_filters>button.active').data('filter_type');
+    let sort = $(this).data('search_sort');
+    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type, sort]);
+    $('#search_results').html(data['content']);
+    $('#search_more').html(data['more']);
+});
+
+
 $(document).on('click', '#search_more>button', async function () {
     let search_string = $('#search_formula').val();
+    if(search_string === ''){
+        search_string = '%';
+    }
     let filter_type = $('#search_filters>button.active').data('filter_type');
+    let sort = $('#search_sort').data('search_sort');
     let request_num = $(this).data('request_num');
-    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type, request_num]);
+    data = await run_method('catalog\\search', 'find_docs', [search_string, filter_type, sort, request_num]);
     $('#search_results').append(data['content']);
     $('#search_more').html(data['more']);
 });
--- js/viewer.js
+++ js/viewer.js
@@ -15,7 +15,8 @@ $("#doc_viewport>div").on("mousedown", function (e) {
 
 $(document).on("mousemove", function (e) {
     if ((mouse_down) && (moveable)) {
-        let m = {x: e.pageX - innerX, y: e.pageY - innerY};
+        let width_mod = ($('#doc_viewport>div').css('width').replace('px', '') / 2),
+            m = {x: (e.pageX - innerX) + width_mod, y: (e.pageY - innerY)};
         $('#doc_viewport>div').css({left: m.x, top: m.y});
     }
 });
@@ -43,13 +44,16 @@ $('#doc_viewport>div').bind('mousewheel', function (e) {
 });
 
 function zoom_in() {
-    let new_zoom = '100%',
+    let new_zoom = 1,
             current_zoom = define_zoom();
-    if (current_zoom < 500) {
-        new_zoom = current_zoom + 25;
-        $('#doc_zoom_scale').html(new_zoom);
-        $('#doc_viewport>div').css('height', new_zoom + '%');
-        if (new_zoom > 100) {
+    if (current_zoom < 3) {
+        new_zoom = current_zoom + 0.25;
+        new_height = $('#doc_viewport>div').data('base_height') * new_zoom;
+        new_width = $('#doc_viewport>div').data('base_width') * new_zoom;
+        $('#doc_zoom_scale').html(new_zoom * 100);
+        $('#doc_viewport>div').css('height', new_height + 'px');
+        $('#doc_viewport>div').css('width', new_width + 'px');
+        if (new_zoom > 1) {
             moveable = true;
         } else {
             moveable = false;
@@ -60,13 +64,16 @@ function zoom_in() {
 function zoom_out() {
     let new_zoom = '100%',
             current_zoom = define_zoom();
-    if (current_zoom >= 125) {
-        new_zoom = current_zoom - 25;
-        $('#doc_zoom_scale').html(new_zoom);
-        $('#doc_viewport>div').css('height', new_zoom + '%');
+    if (current_zoom >= 1.25) {
+        new_zoom = current_zoom - 0.25;
+        new_height = $('#doc_viewport>div').data('base_height') * new_zoom;
+        new_width = $('#doc_viewport>div').data('base_width') * new_zoom;
+        $('#doc_zoom_scale').html(new_zoom * 100);
+        $('#doc_viewport>div').css('height', new_height + 'px');
+        $('#doc_viewport>div').css('width', new_width + 'px');
         $('#doc_viewport>div').css('top', 0);
         $('#doc_viewport>div').css('left', 0);
-        if (new_zoom <= 100) {
+        if (new_zoom <= 1) {
             moveable = false;
         } else {
             moveable = true;
@@ -75,7 +82,8 @@ function zoom_out() {
 }
 
 $(document).on('click', '#doc_zoom_reset', function () {
-    $('#doc_viewport>div').css('height', '100%');
+    $('#doc_viewport>div').css('height', $('#doc_viewport>div').data('base_height') + 'px');
+    $('#doc_viewport>div').css('width', $('#doc_viewport>div').data('base_width') + 'px');
     $('#doc_viewport>div').css('top', 0);
     $('#doc_viewport>div').css('left', 0);
     $('#doc_zoom_scale').html('100');
@@ -85,7 +93,12 @@ $(document).on('click', '#doc_zoom_reset', function () {
 function define_zoom() {
     let height = $('#doc_viewport').css('height').replace('px', ''),
             current_height = $('#doc_viewport>div').css('height').replace('px', ''),
-            current_zoom = current_height / height * 100;
+            current_width = $('#doc_viewport>div').css('width').replace('px', '');
+            current_zoom = current_height / height;
+            if(current_zoom === 1){
+                $('#doc_viewport>div').data('base_height', current_height);
+                $('#doc_viewport>div').data('base_width', current_width);
+            }
 
     return current_zoom;
 }
